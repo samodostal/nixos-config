b9f65e74c045ad5af32f30f8b0e582c9b08273a0
samodostal <samuel.dostal.sk@gmail.com>
2024-09-04 20:38:27 +0200

    Change kitty padding

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -1,6 +1,9 @@
 {
   programs.kitty = {
     enable = true;
+    settings = {
+      window_padding_width = 0;
+    };
     keybindings = {
       "ctrl+plus" = "change_font_size all +2.0";
       "ctrl+minus" = "change_font_size all -2.0";
